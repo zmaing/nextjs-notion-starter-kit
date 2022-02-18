2ce06aa293f1461df6780bfcaeeb3b682719785c
zmaing <zmaing@163.com>
2022-02-19 00:33:32 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -26,7 +26,7 @@ export const Footer: React.FC<{
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2021 {config.author}</div>
+      <div className={styles.copyright}>Copyright 2021 {config.author} <a href="https://beian.miit.gov.cn/">京ICP备19029292号-1</a></div>
 
       {hasMounted ? (
         <div className={styles.settings}>
